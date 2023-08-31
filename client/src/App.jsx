d4bfc34d11615236576c9fe2e473d5ba242888f2
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2023-08-30 20:33:00 -0400

    update dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,15 @@
 import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/users")
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+  
   return <></>;
 };
 
