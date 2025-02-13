1d3b6ebc6a2bd9e60ab33ea24c342a93749ef236
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-02-13 12:34:23 -0500

    fix for loggin and pm2 start

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -4,15 +4,14 @@ const db_uri =
   process.env.NODE_ENV === "production"
     ? process.env.DB_URI
     : process.env.DB_URI_DEV;
+console.log(db_uri);
 
 mongoose
   .connect(`${db_uri}${db}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() =>
-    console.log(`Connected to: ${db_uri}`, `Established a connection to ${db}`)
-  )
+  .then(() => console.log(`Established a connection to ${db}`))
   .catch((err) =>
     console.log(`Something went wrong when connecting to ${db}`, err)
   );
