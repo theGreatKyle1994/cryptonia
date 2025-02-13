01cfac0afaecb2ca68b7a24c038de859154be44d
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-02-13 12:48:45 -0500

    fix env issue on pm2 start

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const app = express();
-dotenv.config();
 const port = process.env.PORT || 8000;
+
+require("dotenv").config();
+require("./config/mongoose.config");
 
 app.use(
   cors({
@@ -16,7 +17,6 @@ app.use(
   express.urlencoded({ extended: true })
 );
 
-require("./config/mongoose.config");
 require("./routes/user.routes")(app);
 
 app.listen(port, () => console.log(`Server live on port: ${port}`));
