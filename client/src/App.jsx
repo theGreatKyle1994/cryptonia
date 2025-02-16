655f3c0063e899f4251dcf984830c458f80fd2a2
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-02-16 18:17:19 -0500

    move header logic into header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 import Table from "./components/Table";
 import LoginRegForm from "./components/LoginRegForm";
@@ -8,29 +8,12 @@ export const globalContext = createContext();
 
 const App = () => {
   const [userData, setUserData] = useState(undefined);
-  const location = useLocation();
-
-  const headerName = (path) => {
-    switch (path) {
-      case "/home":
-        return "Home";
-      case "/favorites":
-        return "Favorites";
-      case "/login":
-        return "Sign-In";
-      case "/register":
-        return "Create an Account";
-      case "/profile":
-        return "Profile";
-    }
-  };
 
   const checkAuth = () => (sessionStorage.getItem("userData") ? true : false);
 
   return (
     <globalContext.Provider value={{ userData, setUserData }}>
       <Header />
-      <h2>{headerName(location.pathname)}</h2>
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
         <Route
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -14,62 +14,80 @@ const Header = () => {
     navigate("/home");
   };
 
+  const headerName = (path) => {
+    switch (path) {
+      case "/home":
+        return "Home";
+      case "/favorites":
+        return "Favorites";
+      case "/login":
+        return "Sign-In";
+      case "/register":
+        return "Create an Account";
+      case "/profile":
+        return "Profile";
+    }
+  };
+
   return (
-    <header>
-      <h1>Cryptonia</h1>
-      <nav>
-        {userData && (
-          <span id="welcome-user">Signed-In: {userData.username}</span>
-        )}
-        <div>
-          {location.pathname == "/home" && !userData && (
-            <Link to={"/login"}>
-              <button type="submit">Login</button>
-            </Link>
+    <>
+      <header>
+        <h1>Cryptonia</h1>
+        <nav>
+          {userData && (
+            <span id="welcome-user">Signed-In: {userData.username}</span>
           )}
-          {location.pathname == "/home" && userData && (
-            <>
-              <Link to={"/favorites"}>
-                <button type="submit">Favorites</button>
+          <div>
+            {location.pathname == "/home" && !userData && (
+              <Link to={"/login"}>
+                <button type="submit">Login</button>
               </Link>
-              <Link to={"/profile"}>
-                <button type="submit">Profile</button>
-              </Link>
-              <button onClick={logout}>Logout</button>
-            </>
-          )}
-          {location.pathname == "/favorites" && userData && (
-            <>
+            )}
+            {location.pathname == "/home" && userData && (
+              <>
+                <Link to={"/favorites"}>
+                  <button type="submit">Favorites</button>
+                </Link>
+                <Link to={"/profile"}>
+                  <button type="submit">Profile</button>
+                </Link>
+                <button onClick={logout}>Logout</button>
+              </>
+            )}
+            {location.pathname == "/favorites" && userData && (
+              <>
+                <Link to={"/home"}>
+                  <button type="submit">Home</button>
+                </Link>
+                <Link to={"/profile"}>
+                  <button type="submit">Profile</button>
+                </Link>
+                <button onClick={logout}>Logout</button>
+              </>
+            )}
+            {location.pathname == "/login" && (
               <Link to={"/home"}>
                 <button type="submit">Home</button>
               </Link>
-              <Link to={"/profile"}>
-                <button type="submit">Profile</button>
-              </Link>
-              <button onClick={logout}>Logout</button>
-            </>
-          )}
-          {location.pathname == "/login" && (
-            <Link to={"/home"}>
-              <button type="submit">Home</button>
-            </Link>
-          )}
-          {location.pathname == "/register" && (
-            <Link to={"/home"}>
-              <button type="submit">Home</button>
-            </Link>
-          )}
-          {location.pathname == "/profile" && (
-            <>
+            )}
+            {location.pathname == "/register" && (
               <Link to={"/home"}>
                 <button type="submit">Home</button>
               </Link>
-              <button onClick={logout}>Logout</button>
-            </>
-          )}
-        </div>
-      </nav>
-    </header>
+            )}
+            {location.pathname == "/profile" && (
+              <>
+                <Link to={"/home"}>
+                  <button type="submit">Home</button>
+                </Link>
+                <button onClick={logout}>Logout</button>
+              </>
+            )}
+          </div>
+        </nav>
+      </header>
+      <h2>{headerName(location.pathname)}</h2>
+    </>
   );
 };
 
